518665715fd92c506dab1e3bc6757f485da8c89e
Sneha Devadiga <sneha.devadiga@clairvoyantsoft.com>
2022-09-23 22:36:09 +0530

    CLAIRTHON-55 API to list single candidate or all candidate details

---
+++ leetQuestPool/lambdas/ListCandidateDetails.py
@@ -0,0 +1,50 @@
+import json
+import boto3
+from decimal import Decimal
+
+TABLE = 'CandidateRegistrationDetails'
+
+def lambda_handler(event, context):
+
+    query = event['query']
+    candidateId = query.get('candidateId', None)
+    if candidateId != None:
+        response = getCandidate(candidateId)
+    else:
+        response = getAllCandidates()
+            
+    print(response)
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-*': '*'
+        },
+        'body': json.dumps(response, cls=DecimalEncoder)
+    }
+
+def getCandidate(candidateId):
+	item = {
+		'id': candidateId
+	}
+	candidateDetail = getItem(item)
+	return candidateDetail['Item']
+
+def getItem(item):
+    dynamodb = getDynamoDbResource()
+    table = dynamodb.Table(TABLE)
+    return table.get_item(Key=item)
+
+def getDynamoDbResource():
+    return boto3.resource('dynamodb')
+
+def getAllCandidates():
+	dynamodb = getDynamoDbResource()
+	table = dynamodb.Table(TABLE)
+	response = table.scan()
+	return response['Items']
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return str(obj)
+        return json.JSONEncoder.default(self, obj)
