af0a8375193f3cd030e524bf6ed6e5a5c3c003f4
Sneha Devadiga <sneha.devadiga@clairvoyantsoft.com>
2022-09-30 16:57:25 +0530

    BE : Bug fix in TestCalculator

--- leetQuestPool/lambdas/TestResultCalculator.py
+++ leetQuestPool/lambdas/TestResultCalculator.py
@@ -21,32 +21,32 @@ def lambda_handler(event, context):
     generatedTestData = getTableData('CandidateTestLinkDetails', generatedTestDataItem)
     candidateDataItem = {'id' : candidateDetailsId}
     candidateData = getTableData('CandidateRegistrationDetails', candidateDataItem)
+    
     testScore = {}
     totalQuestions = 0;
-    totalScore = 0;
     techwiseResult = []
     
     totalQuestions = totalQuestions + len(testDetails)
     testResponses, score = getTestScore(testDetails, generatedTestData['testQuestions'])
     techwiseResult.extend(testResponses)
-    totalScore = totalScore + score
+
     
     id = uuid.uuid1().hex
     testScore['candidateId']=body['candidateDetailsId']
     testScore['id'] = id
-    testScore['totalScore'] = totalScore
+    testScore['totalScore'] = score
     testScore['totalQuestions'] = totalQuestions
     testScore['testResult'] = techwiseResult
-    testScore['testStatus'] = getTestStatus(totalScore, totalQuestions)
+    testScore['testStatus'] = getTestStatus(score, totalQuestions)
     testScore['feedbackStatus'] = feedbackUrl+candidateDetailsId
     
     candidateData['result'] = testScore['testStatus']
     
-    body['testTaken']=True
-    body['resultId']=id
+    generatedTestData['testTaken']=True
+    generatedTestData['resultId']=id
     
     persistCandidateTestDetails('CandidateTestResults', testScore)
-    persistCandidateTestDetails('CandidateTestLinkDetails', body)
+    persistCandidateTestDetails('CandidateTestLinkDetails', generatedTestData)
     persistCandidateTestDetails('CandidateRegistrationDetails', candidateData)
     
     publishToSlack(body['name'], body['email'], testScore)
@@ -88,7 +88,8 @@ def getTestScore(testResults, generatedTestData):
     score = 0
     for result in testResults:
         answer = getAnswer(result['questionId'], generatedTestData)
-        if (answer == result['candidateAnswer']):
+        candidateAnswer = result['candidateAnswer']
+        if answer.strip() == candidateAnswer.strip():
             result['correctness'] = "correct"
             score = score + 1
         else:
@@ -98,7 +99,6 @@ def getTestScore(testResults, generatedTestData):
 def getAnswer(id, generatedTestData):
     for entry in generatedTestData:
         if entry['questionId'] == id:
-            print(entry['questionId'])
             return entry['answer']
 
 def getTestStatus(totalScore, totalQuestions):
